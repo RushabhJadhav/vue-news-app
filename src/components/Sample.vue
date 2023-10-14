3b41cecfc724a56376e6f3bf9ff4c8ee09f81600
Rushabh Jadhav <rushabhjadhav666@gmail.com>
2023-10-15 00:23:27 +0530

    inital setup

---
+++ src/components/Sample.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <h2 class="text-primary m-1" >{{ count }}</h2>
+        <button @click="increment">Increment</button>
+        <button @click="decrement">Decrement</button>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            count: 0
+        }
+    },
+    methods: {
+        increment() {
+            this.count++;
+        },
+        decrement() {
+            if(this.count > 0) {
+                this.count--;
+            }
+        }
+    }
+}
+</script>
+
+<style scoped lang="scss">
+</style>
---
+++ src/modules/HomeView.vue
@@ -0,0 +1,14 @@
+<template>
+    <Sample />
+</template>
+
+<script>
+import Sample from '../components/Sample.vue';
+
+export default {
+    components: {
+        Sample,
+    }
+}
+</script>
+
