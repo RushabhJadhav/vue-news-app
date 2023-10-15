04a5d54878775567d91e751d81b509919ad774c4
Rushabh Jadhav <rushabhjadhav666@gmail.com>
2023-10-15 09:36:10 +0530

    api added

---
+++ src/components/Header.vue
@@ -0,0 +1,60 @@
+<template>
+    <header>
+        <h1 class="logo">News App</h1>
+        <ul>
+            <!-- <li><a href="">Home</a></li>
+            <li><a href="">More</a></li>
+            <li><a href="">More</a></li>
+            <li><a href="">More</a></li>
+            <li><a href="">More</a></li>     -->
+            <li><RouterLink to="/" >home</RouterLink></li>    
+            <li><RouterLink to="/about" >more</RouterLink></li>    
+            <li><RouterLink to="/about" >more</RouterLink></li>    
+            <li><RouterLink to="/about" >more</RouterLink></li>    
+            <li><RouterLink to="/about" >more</RouterLink></li>    
+        </ul>
+        <div class="searchBar">
+            <input type="text">
+        </div>
+    </header>
+</template>
+
+<script>
+import { RouterLink } from 'vue-router';
+
+export default {
+    components: { RouterLink }
+}
+</script>
+
+<style scoped lang="scss">
+header {
+    display: flex;
+    flex-direction: row;
+    border: 1px solid red;
+    align-items: center;
+    justify-content: space-between;
+    padding: 20px 10%;
+    margin-bottom: 20px;
+
+    ul {
+        display: inherit;
+
+        li {
+            list-style-type: none;
+            margin: 0 20px;
+            font-size: 1.3rem;
+
+            a {
+                text-decoration: none;
+                color: black;
+            }
+        }
+    }
+
+    input {
+        height: 30px;
+        width: 300px;
+    }
+}
+</style>
--- src/components/Sample.vue
+++ src/components/Sample.vue
@@ -1,17 +1,33 @@
 <template>
-    <div>
-        <h2 class="text-primary m-1" >{{ count }}</h2>
-        <button @click="increment">Increment</button>
-        <button @click="decrement">Decrement</button>
+    <Header></Header>
+    <div class="news-cont">
+        <div class="news-card" v-for="(item, index) in data">
+            <h2>{{ item.title }}</h2>
+            <img class="news-image" :src="item.urlToImage" alt="image not available">
+            <p>{{ item.source.name }}</p>
+            <p>Published On : {{ item.publishedAt }}</p>
+            <p>Author : {{ item.author }}</p>
+            <p>{{ item.description }} <a :href="item.url" target="_blank">read more...</a></p>
+        </div>
     </div>
 </template>
 
 <script>
+import Header from './Header.vue';
+
 export default {
     data() {
         return {
-            count: 0
+            count: 0,
+            apiKey: '6e66a5a2bd884fc5ba13195ed5187194',
+            data: ''
         }
+    },
+    components: {
+        Header,
+    },
+    mounted() {
+        this.callAPI()
     },
     methods: {
         increment() {
@@ -21,10 +37,43 @@ export default {
             if(this.count > 0) {
                 this.count--;
             }
+        },
+        callAPI() {
+            fetch(`https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=${this.apiKey}`)
+            .then(response => response.json())
+            .then(data => {
+                this.data = data.articles
+            })
         }
     }
 }
 </script>
 
 <style scoped lang="scss">
+.news-cont {
+    width: 60%;
+    margin: auto;
+
+    .news-card {
+        // border: 1px solid red;
+
+        h2 {
+            margin: 10px 0 20px 0;
+            text-align: center;
+        }
+
+        p {
+            padding: 10px 0;
+
+            a {
+                text-decoration: none;
+                color: inherit
+            }
+        }
+
+        .news-image {
+            width: 100%;
+        }
+    }
+}
 </style>
